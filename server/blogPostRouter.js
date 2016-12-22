dd5db646d209a27e4addd92561b41dcf4486b8ee
Michiya Hibino <mphibino@gmail.com>
2016-12-21 22:21:50 -0800

    added route handling functions in blogPostRouter

--- server/blogPostRouter.js
+++ server/blogPostRouter.js
@@ -7,13 +7,63 @@ const jsonParser = bodyParser.json();
 const {BlogPosts} = require('./models');
 
 // add content
-BlogPosts.create( /* Fill in here */ );
-BlogPosts.create( /* Fill in here */ );
+BlogPosts.create( 
+	'first post', 'today it was raining in southern california', 'Lala Schifrin');
+BlogPosts.create( 
+	'post with the most', 'sometimes less is more', 'Will Fillmore');
+// title, content, author, publishDate
 
 // create endpoints
-router.get( /* Fill in here */ );
-router.post( /* Fill in here */ );
-router.delete( /* Fill in here */ );
-router.put( /* Fill in here */ );
+router.get( '/', (req, res) => {
+	res.json(BlogPosts.get());
+});
+
+router.post( '/', jsonParser, (req, res) => {
+	const requiredFields = [title, content, author];
+	for (let i = 0; i < requiredFields.length; i++) {
+		const field = requiredFields[i];
+		if (!(field in req.body)) {
+			const message = `Missing \`${field}\` in body of http request`
+			console.error(message);
+			return res.status(400).send(message);
+		}
+	}
+	const posting = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+	res.status(201).json(posting);
+});
+
+router.delete('/:id', (req, res) => {
+	BlogPosts.delete(req.params.id);
+	console.log(`Deleted blog post entry `\${req.params.id}\``);
+	res.status(204).end();
+});
+
+router.put('/:id', jsonParser, (req, res) => {
+	const requiredFields = [title, content, author];
+	for (let i = 0; i < requiredFields.length; i++) {
+		const field = requiredFields[i];
+		if (!(field in req.body)) {
+			const message = `Missing \`${field}\` in body of http request`
+			console.error(message);
+			return res.status(400).send(message);
+		}
+	}
+	if (req.params.id !== req.body.id) {
+		const message = (`request path id ${req.params.id}`
+			`and request body id ${req.body.id} must match`);
+		console.error(message);
+		return res.status(400).send(message);
+	}
+	console.log(`Updating blog post entry ${req.body.id}`);
+	const updatedPost = BlogPosts.update({
+		id: req.params.id,
+		title: req.body.title,
+		content: req.body.content,
+		author: req.body.author,
+		publishDate: req.body.publishDate || Date.now()
+	});
+	res.status(204).json(updatedPost);
+
+});
 
 module.exports = router;
